9ddfb12c14f26735e440bcd88dc247f2fe7a2123
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-11 14:06:37 +0100

    fix(savage65): Added the switch DISABLE_LAYER_TRACKING The keyboard becomes unresponsive when implementing layer functions and thus the code relative to layer tracking can now be disabled

--- keyboards/cannonkeys/savage65/keymaps/chrisgve/keymap.c
+++ keyboards/cannonkeys/savage65/keymaps/chrisgve/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_65_ansi_blocker_tsangan_split_bs(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
-		QK_BOOT, MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+		QK_BOOT, MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,           _______,
 		KC_CAPS, DF_M_P,  DF_L_P,  DF_W_P,  DF_G_P,  _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,          _______,
 		_______,          DF_MAC,  DF_LNX,  DF_WIN,  DF_GMG,  _______, _______, KC_MUTE, _______, _______, _______, _______, KC_VOLU,          _______,
 		_______, _______, _______,                                     KC_MPLY,                                     KC_MPRV, KC_VOLD, KC_MNXT, _______
